442b8a538a11d43c66ee4f97873be4c8aad88439
Chirag <sharma.chirag3737@gmail.com>
2021-02-13 20:16:18 +0530

    Updates

---
+++ precache-manifest.dc058d61ef691c2923af76b7ce779a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fafcf77a5005413591fe9c427e8377",
+    "url": "/shopitFrontEnd/index.html"
+  },
+  {
+    "revision": "9c92b06e796c11336e60",
+    "url": "/shopitFrontEnd/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "1f8465d97ce30531319c",
+    "url": "/shopitFrontEnd/static/css/main.2a7dda11.chunk.css"
+  },
+  {
+    "revision": "9c92b06e796c11336e60",
+    "url": "/shopitFrontEnd/static/js/2.89d58d09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopitFrontEnd/static/js/2.89d58d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8465d97ce30531319c",
+    "url": "/shopitFrontEnd/static/js/main.9195e48a.chunk.js"
+  },
+  {
+    "revision": "620e01ecf9e86fd7c1e6",
+    "url": "/shopitFrontEnd/static/js/runtime-main.55d25ea0.js"
+  }
+]);
