d709e4d1aab4b73d5673a8fff0729a4826050fd8
Chirag <sharma.chirag3737@gmail.com>
2021-02-13 20:19:36 +0530

    random commit

--- src/core/Home.js
+++ src/core/Home.js
@@ -6,34 +6,36 @@ import { toast } from "react-toastify";
 import ProductCard from "./ProductCard";
 
 const Home = () => {
-	const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-	const preload = () => {
-		getProducts().then((data) => {
-			if (data.error) {
-				return toast.error(data.error);
-			}
-			setProducts(data);
-			console.log(data);
-		});
-	};
+  const preload = () => {
+    getProducts()
+      .then((data) => {
+        if (data.error) {
+          return toast.error(data.error);
+        }
+        setProducts(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-	useEffect(() => {
-		preload();
-	}, []);
-	return (
-		<Base title='Home' description='Wecome To the Tshirt Store'>
-			<div className='row'>
-				{products.map((product) => (
-					<ProductCard
-						key={product._id}
-						product={product}
-						addtoCartButton='true'
-					/>
-				))}
-			</div>
-		</Base>
-	);
+  useEffect(() => {
+    preload();
+  }, []);
+  return (
+    <Base title="Home" description="Wecome To the Tshirt Store">
+      <div className="row">
+        {products.map((product) => (
+          <ProductCard
+            key={product._id}
+            product={product}
+            addtoCartButton="true"
+          />
+        ))}
+      </div>
+    </Base>
+  );
 };
 
 export default Home;
