e792aed741dcbcbfcb7005e4a26e081d0fed05fd
Chirag Sharma <sharma.chirag3737@gmail.com>
2022-03-23 22:44:33 +0530

    image Size and footer

--- src/core/ImageHelper.js
+++ src/core/ImageHelper.js
@@ -6,7 +6,7 @@ const ImageHelper = ({ product }) => {
 	const imageURL = `${API}/product/photo/${product._id}`;
 
 	return (
-		<CardImg top width='100%' src={imageURL} alt='Product Image Not loaded' />
+		<CardImg top width='100%' src={imageURL} alt='Product Image Not loaded' height={350} />
 	);
 };
 
